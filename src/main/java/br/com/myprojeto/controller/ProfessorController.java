97be68a1a7825e0615df99663b684df4aef6cdff
Diego Cruz <diegodemcruz@gmail.com>
2019-08-19 20:13:17 -0300

    commit

--- src/main/java/br/com/myprojeto/controller/ProfessorController.java
+++ src/main/java/br/com/myprojeto/controller/ProfessorController.java
@@ -3,12 +3,14 @@ package br.com.myprojeto.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import br.com.myprojeto.domain.Professor;
 import br.com.myprojeto.service.ProfessorService;
+import javassist.tools.rmi.ObjectNotFoundException;
 
 @Controller
 @RequestMapping("professor")
@@ -37,6 +39,21 @@ public class ProfessorController {
 		professorService.salvar(professor);
 		return listarTodosProfessores();
 	}
+	
+	
+	@GetMapping ("/alterar/{id}")
+	public ModelAndView alterarProfessor(@PathVariable("id")Integer idProfessor) throws ObjectNotFoundException{
+		ModelAndView mv = new ModelAndView("aluno/alteraProfessor");
+		mv.addObject("professor", professorService.buscarporId(idProfessor));
+		return mv;
+	}
+	
+	@PostMapping("/alterar")
+	public ModelAndView alterar(Professor professorAlterado) throws ObjectNotFoundException{
+		professorService.salvarAlteracao(professorAlterado);
+		return listarTodosProfessores();
+	}
+	
 	
 	
 
--- src/main/java/br/com/myprojeto/service/ProfessorService.java
+++ src/main/java/br/com/myprojeto/service/ProfessorService.java
@@ -1,6 +1,7 @@
 package br.com.myprojeto.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -8,6 +9,7 @@ import org.springframework.stereotype.Service;
 
 import br.com.myprojeto.domain.Professor;
 import br.com.myprojeto.repository.ProfessorRepository;
+import javassist.tools.rmi.ObjectNotFoundException;
 
 @Service
 public class ProfessorService {
@@ -23,4 +25,17 @@ public class ProfessorService {
 	public Professor salvar (Professor professor) {
 		return repoProfessor.save(professor);
 	}
+	
+	public Professor buscarporId(Integer id) throws ObjectNotFoundException{
+		Optional<Professor> professor = repoProfessor.findById(id);
+		return professor.orElseThrow(() -> new ObjectNotFoundException("Professor não Encontrado. Id:" + id));
+	}
+	
+	public Professor salvarAlteracao(Professor professorAlterado) throws ObjectNotFoundException{
+		Professor professor = buscarporId(professorAlterado.getId());
+		professor.setId(professorAlterado.getId());
+		professor.setNome(professorAlterado.getNome());
+		return salvar(professor);
+	}
+	
 }
